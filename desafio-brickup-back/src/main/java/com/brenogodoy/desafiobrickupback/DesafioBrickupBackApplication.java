035b13fb77837ac630aa5ada07962507de7b04b6
Breno Godoy <godoyy25z@gmail.com>
2023-12-19 02:43:08 -0300

    feat: Gera meu backend com Spring initializer e cria a entidade de tarefa

---
+++ desafio-brickup-back/src/main/java/com/brenogodoy/desafiobrickupback/DesafioBrickupBackApplication.java
@@ -0,0 +1,13 @@
+package com.brenogodoy.desafiobrickupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBrickupBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBrickupBackApplication.class, args);
+	}
+
+}
---
+++ desafio-brickup-back/src/test/java/com/brenogodoy/desafiobrickupback/DesafioBrickupBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenogodoy.desafiobrickupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBrickupBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
